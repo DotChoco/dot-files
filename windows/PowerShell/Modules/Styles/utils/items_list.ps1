9c90c4cd8a09232a0a4c80572a357d4da6da39d1
DotChoco <chocoretostonus@gmail.com>
2025-08-04 15:41:44 -0600

    f: menor update

--- windows/PowerShell/Modules/Styles/utils/items_list.ps1
+++ windows/PowerShell/Modules/Styles/utils/items_list.ps1
@@ -1,11 +1,13 @@
 function la {
-  $items = Get-ChildItem
+  param([string]$path)
+  if($path -eq ""){ $dirs = Get-ChildItem }
+  else{ $dirs = Get-ChildItem -Path $path }
   $supportsVT = $Host.UI.SupportsVirtualTerminal
   $icon = $defaultIcon.Icon
   $color = $defaultIcon.Color
   $defaultIcon = [pscustomobject]@{ Icon="`u{f0f6}"; Color=$PSStyle.Foreground.FromRgb(163,163,163) }
 
-  foreach ($it in $items) {
+  foreach ($it in $dirs) {
     if ($it.PSIsContainer) {
       $icon = if ($Host.UI.SupportsVirtualTerminal) { "`u{e5ff}" } # folder icon
       $color = $PSStyle.Foreground.FromRgb(210,210,210)
@@ -20,4 +22,7 @@ function la {
   }
 }
 
-function ll { echo("-----------------") && la && echo("-----------------") }
+function ll {
+  param([string]$path)
+  echo("-----------------") && la $path && echo("-----------------")
+}
