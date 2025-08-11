f70db4ac5d92c4107482850851e0837cca1ffa5d
Dot Choco <chocoretostonus@gmail.com>
2025-08-10 20:19:36 -0600

    f: menor update

--- linux/GameDev/psp_installer/pspdev/psp/include/SDL2/SDL_revision.h
+++
@@ -1,8 +0,0 @@
-/* #undef SDL_VENDOR_INFO */
-#define SDL_REVISION_NUMBER 0
-
-#ifdef SDL_VENDOR_INFO
-#define SDL_REVISION "SDL-release-2.32.2-0-ge11183ea6 (" SDL_VENDOR_INFO ")"
-#else
-#define SDL_REVISION "SDL-release-2.32.2-0-ge11183ea6"
-#endif
--- linux/GameDev/psp_installer/pspdev/psp/include/zzip/stdint.h
+++
@@ -1,8 +0,0 @@
-#ifndef _ZZIP__STDINT_H
-#define _ZZIP__STDINT_H 1
-
-/* obsolete header - use zzip/cstdint.h instead */
-
-#include <zzip/cstdint.h>
-
-#endif /*_ZZIP_STDINT_H*/
