d7cbc891c4f22f1b9dc11d165505ed8b06b6b981
DotChoco <chocoretostonus@gmail.com>
2025-08-22 17:58:22 -0600

    f: Print Auths

--- windows/PowerShell/Modules/FAPI/core/Gvars.ps1
+++ windows/PowerShell/Modules/FAPI/core/Gvars.ps1
@@ -1,32 +1,68 @@
-$CID  = "" #Client ID
-$CSEC = "" #Client Secret
-$ATOK = "" #Access Token
-$RTOK = "" #Refresh Token
-$SAUTH= "" #Server Auth
+$SAUTH = "" #Server Auth
+$ANAME = "" #Client ID
+$ADESC = "" #Client ID
+$CID   = "" #Client ID
+$CSEC  = "" #Client Secret
+$ATOK  = "" #Access Token
+$RTOK  = "" #Refresh Token
 
 function SelectAuth{
   param([int]$index)
   # Config file Auth Apps
   $Auths = Get-Content -Path "$PSScriptRoot/conf.json" -Raw | ConvertFrom-Json
 
-  # $inputStr = Read-Host "Write the index of auth you want to use"
-  # Convert to int
-  # [int]$index = $inputStr
-
+  $script:SAUTH = $Auths[$index].sauth
+  $script:ANAME = $Auths[$index].name
+  $script:ADESC = $Auths[$index].desc
   $script:CID   = $Auths[$index].cid
   $script:CSEC  = $Auths[$index].csec
   $script:ATOK  = $Auths[$index].atok
   $script:RTOK  = $Auths[$index].rtok
-  $script:SAUTH = $Auths[$index].sauth
 
 }
 
+
+function PrintAuths{
+  # Config file Auth Apps
+  $Auths = Get-Content -Path "$PSScriptRoot/conf.json" -Raw | ConvertFrom-Json
+
+  # $AuthsFiltered = FilterAuths $Auths
+  $AuthsFiltered = $Auths
+  $SAUTHMAP = @{
+    0 = "Local"
+    1 = "Production"
+    2 = "MiContador"
+  }
+  for ($i = 0; $i -lt $AuthsFiltered.Length; $i++) {
+    $ServerName = if ($SAUTHMAP.ContainsKey($SAUTH)) { $SAUTHMAP[$SAUTH] } else { $SAUTHMAP[1] }
+    Write-Host "-----------------"
+    Write-Host "Server:" $ServerName
+    Write-Host "Name:" $AuthsFiltered[$i].name
+    Write-Host "Description:" $AuthsFiltered[$i].desc
+    Write-Host "Client ID:" $AuthsFiltered[$i].cid
+    Write-Host "Client Secret:" $AuthsFiltered[$i].csec
+  }
+  Write-Host "-----------------"
+
+}
+
+function FilterAuths{
+  param([object[]]$auths)
+  $authsName = FilterAuthsN $auths
+  $authsName = FilterAuthsN $auths
+  $authsName = FilterAuthsN $auths
+
+}
+
+
 function ClearAuth{
+  $script:SAUTH = ""
+  $script:ANAME = ""
+  $script:ADESC = ""
   $script:CID   = ""
   $script:CSEC  = ""
   $script:ATOK  = ""
   $script:RTOK  = ""
-  $script:SAUTH = ""
 }
 
 $URLMAP = @{
