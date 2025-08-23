31896d21cb6fb8e89e8ec196376b92d6406e8049
Dot Choco <chocoretostonus@gmail.com>
2025-08-23 11:14:49 -0600

    f: menor update

--- winInstall.ps1
+++ winInstall.ps1
@@ -1,10 +1,11 @@
 $REPODIR = Get-Location
 $SHARED = "$REPODIR/shared"
 $WIN = "$REPODIR/windows"
+$HOMEDIR = ""
 
 # Create the symbolic for the powershell directory
-rm "$HOME/Documents/PowerShell/" -Force -Recurse
-New-Item -ItemType Junction -Path "$HOME/Documents/PowerShell/" -Target "$WIN/PowerShell/"
+rm "$HOMEDIR/Documents/PowerShell/" -Force -Recurse
+New-Item -ItemType Junction -Path "$HOMEDIR/Documents/PowerShell/" -Target "$WIN/PowerShell/"
 
 # Create the symbolic relation for the nvim directory
 if(Test-Path -Path "$env:LOCALAPPDATA/nvim/") { rm "$env:LOCALAPPDATA/nvim/" -Force -Recurse }
@@ -19,7 +20,7 @@ cp "$SHARED/Fonts/CaskaydiaCove/*" "C:/Windows/Fonts/"
 
 # Git will discard all changes that could be made into this files
 git update-index --assume-unchanged .\shared\nvim\lazy-lock.json
-git update-index --assume-unchanged windows/PowerShell/Microsoft.PowerShell_profile.ps1
+git update-index --assume-unchanged windows/PowerShell/conf.json
 
 Remove-Variable WIN -ErrorAction SilentlyContinue
 Remove-Variable SHARED -ErrorAction SilentlyContinue
