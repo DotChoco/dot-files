b994e4175cb715be28830bc28cab92e5a17ef6f2
Dot Choco <chocoretostonus@gmail.com>
2025-10-04 12:13:14 -0600

    f: menor update

--- uninstall.ps1
+++ uninstall.ps1
@@ -1,8 +1,6 @@
 $json = Get-Content -Path "$env:LOCALAPPDATA/Powershell/conf.json" -Raw | ConvertFrom-Json
 
 # Global Mutables Variables
-$jhome = $json.homedir
-$jdev = $json.devdir
 $jpws = $json.pwsdir
 $jvi = $json.vidir
 
@@ -10,8 +8,6 @@ $jvi = $json.vidir
 Write-Host "`n`n`n`n"
 Write-Host "Purgando Datos"
 
-Remove-Item $jdev -Force -Recurse
-Remove-Item $jpws -Force -Recurse
 Remove-Item $jvi -Force -Recurse
 Remove-Item $jhome -Force -Recurse
 Remove-Item "$env:LOCALAPPDATA/Powershell/conf.json" -Force -Recurse
@@ -20,8 +16,8 @@ Remove-Item "$env:LOCALAPPDATA/nvim-data" -Force -Recurse
 
 Write-Host "`n`n`n`n"
 Write-Host "Borrando informacion de Git"
-git config --global --unset user.name
-git config --global --unset user.email
+git config set --global user.name ""
+git config set --global user.email ""
 Remove-Item "$env:USERPROFILE/.gitconfig" -Force -Recurse
 Remove-Item "$env:USERPROFILE/.git-credentials" -Force -Recurse
 Remove-Item "$env:USERPROFILE/.ssh" -Force -Recurse
